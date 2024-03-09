602e33b834a73dbcf04443541151d4b79f508645
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-03-10 00:44:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ExpenseItem from './Components/ExpenseItem'
+
+function App(){
+  return (
+    <>
+    <h1>Hello</h1>
+     <ExpenseItem></ExpenseItem>
+    </>
+  )
+}
+export default App
---
+++ src/Components/ExpenseItem.jsx
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+return (
+  <div>
+    <div>Food RS 10</div>
+    <div>Petrol RS 100</div>
+    <div>Movie RS 200</div>
+  </div>
+);
+}
+export default ExpenseItem
