dfafeed322ea620651f7e013afc40651c3ac72a0
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-03-10 21:06:51 +0530

    now i do some correction my code

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import ExpenseItem from './Components/ExpenseItem'
 function App(){
   return (
     <>
-    <h1>Hello</h1>
-     <ExpenseItem></ExpenseItem>
+      <h1>Hello</h1>
+      <ExpenseItem />
+      <ExpenseItem />
     </>
-  )
+  );
 }
 export default App
--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,19 +1,19 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
-  const amountSpent = 100;
-  const locationOfExpenditure = "Grocery Store";
 
+function ExpenseItem() {
+  const expenseDate = "2024-03-23";
+  const expenseRate = '$294.67';
+  const expenseTitle = 'Grocery Shop'
+  const locationOfExpenditure = "Uttar Pardesh";
   return (
     <>
       <div className="expense-item">
-        <div>Food RS 10</div>
+        <div>{expenseDate}</div>
         <div className="expense-item__description">
-          <h2>Petrol RS 100</h2>
+          <h2>{expenseTitle}</h2>
+          <h2>{locationOfExpenditure}</h2>
+          <div className="expense-item__price">{expenseRate}</div>
         </div>
-        <div className="expense-item__price">Movie RS 200</div>
-      </div>
-      <div id="expenseInfo">
-        You spent ${amountSpent} at {locationOfExpenditure}.
       </div>
     </>
   );
