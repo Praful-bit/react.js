df674d783fa49f32d2120d5610ca9727ff130cb9
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-03-10 15:12:42 +0530

    i add this css component for some styling and i add locationOfExpenditure for track spent amount

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,10 +1,21 @@
-function ExpenseItem(){
-return (
-  <div>
-    <div>Food RS 10</div>
-    <div>Petrol RS 100</div>
-    <div>Movie RS 200</div>
-  </div>
-);
+import "./ExpenseItem.css";
+function ExpenseItem() {
+  const amountSpent = 100;
+  const locationOfExpenditure = "Grocery Store";
+
+  return (
+    <>
+      <div className="expense-item">
+        <div>Food RS 10</div>
+        <div className="expense-item__description">
+          <h2>Petrol RS 100</h2>
+        </div>
+        <div className="expense-item__price">Movie RS 200</div>
+      </div>
+      <div id="expenseInfo">
+        You spent ${amountSpent} at {locationOfExpenditure}.
+      </div>
+    </>
+  );
 }
-export default ExpenseItem
+export default ExpenseItem;
